9199760a8d6388801984f65087dc377976fe6da5
Heider <heider2112@gmail.com>
2024-06-09 23:38:59 -0400

    server side is not supported only static

--- actions/sendEmail.tsx
+++ actions/sendEmail.tsx
@@ -1,8 +1,8 @@
-"use server";
-export const sendEmail = async (formData: FormData) => {
-
-        const senderEmail = formData.get("senderEmail");
-        const message = formData.get("message");
-        console.log(senderEmail);
-        console.log(message);
-    }
+// "use server";
+// export const sendEmail = async (formData: FormData) => {
+//
+//         const senderEmail = formData.get("senderEmail");
+//         const message = formData.get("message");
+//         console.log(senderEmail);
+//         console.log(message);
+//     }
--- components/contact.tsx
+++ components/contact.tsx
@@ -37,37 +37,36 @@ export default function Contact() {
                 <a className="underline" href="mailto:heider2112@gmail.com">
                     heider2112@gmail.com
                 </a>{" "}
-                or through this form.
             </p>
 
-            <form
-            className="mt-10 flex flex-col"
-            action={async(formData) => {
-                await sendEmail(formData);
-            }}
-            >
+            {/*<form*/}
+            {/*className="mt-10 flex flex-col"*/}
+            {/*action={async(formData) => {*/}
+            {/*    await sendEmail(formData);*/}
+            {/*}}*/}
+            {/*>*/}
 
-                <input
-                    className="h-14 px-4 rounded-lg borderBlack transition-all"
-                    name="senderEmail"
-                    type="email"
-                    required
-                    maxLength={500}
-                    placeholder="Your email"
-                />
-                <textarea
-                    className="h-52 my-3 rounded-lg borderBlack p-4 transition-all"
-                    name="message"
-                    placeholder="Your message"
-                    required
-                    maxLength={5000}
-                />
+            {/*    <input*/}
+            {/*        className="h-14 px-4 rounded-lg borderBlack transition-all"*/}
+            {/*        name="senderEmail"*/}
+            {/*        type="email"*/}
+            {/*        required*/}
+            {/*        maxLength={500}*/}
+            {/*        placeholder="Your email"*/}
+            {/*    />*/}
+            {/*    <textarea*/}
+            {/*        className="h-52 my-3 rounded-lg borderBlack p-4 transition-all"*/}
+            {/*        name="message"*/}
+            {/*        placeholder="Your message"*/}
+            {/*        required*/}
+            {/*        maxLength={5000}*/}
+            {/*    />*/}
 
-                <button type="submit"
-                className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 disabled:scale-100 disabled:bg-opacity-65"
-                >Submit <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1"/>{" "}</button>
+            {/*    <button type="submit"*/}
+            {/*    className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 disabled:scale-100 disabled:bg-opacity-65"*/}
+            {/*    >Submit <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1"/>{" "}</button>*/}
 
-            </form>
+            {/*</form>*/}
 
         </motion.section>
     );
